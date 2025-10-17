fcd71b6c85ed856f094ac780d7a1aeb9a2af90fc
Darya <daryapogas@gmail.com>
2025-10-17 17:28:55 -0500

    Implement create & update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
  root "listings#index"
 
-  resources :listings, only: [:index, :show] do
+  resources :listings do
     resources :images,   only: [:index]   # /listings/:listing_id/images
-    resources :reviews,  only: [:index]   # /listings/:listing_id/reviews
+    resources :reviews,  only: [:index, :new, :create, :edit, :update]   # /listings/:listing_id/reviews
     resources :bookings, only: [:index]   # /listings/:listing_id/bookings
   end
 
